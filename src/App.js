30e22015c19b99c9562a02a19e6d68cff70abea8
Jarno <jarnobogaert9@gmail.com>
2019-02-07 14:19:52 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,155 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import InsertForm from './Components/InsertForm';
+import Contacts from './Components/Contacts';
+import EditForm from './Components/EditForm';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      contacts: [
+        {
+          name: 'Jarno Bogaert',
+          // Not my real phone number ;)
+          number: '+324 78 123 429'
+        },
+        {
+          name: 'Will Smith',
+          number: '999 999 999'
+        }
+      ],
+      newName: '',
+      newNumber: '',
+      updateName: '',
+      updateNumber: '',
+      contact: {}
+    }
+  }
+
+  // Set state of the name the user will insert
+  newNameFunc = (e) => {
+    this.setState({
+      newName: e.target.value
+    })
+  }
+
+  // Set state of the number the user will insert
+  newNumberFunc = (e) => {
+    this.setState({
+      newNumber: e.target.value    
+    })
+  }
+
+  // Insert the name & number from the state
+  submitForm = (e) => {
+    e.preventDefault();
+    if (!(this.state.newName === '' || this.state.newNumber === '')) {
+      let newContacts = this.state.contacts;
+      newContacts.push({
+        name: this.state.newName,
+        number: this.state.newNumber
+      });
+      this.setState({
+        contacts: newContacts,
+        newName: '',
+        newNumber: ''
+      });
+    }
+  }
+
+  removeContact = (i) => {
+    let contacts = this.state.contacts;
+    contacts.splice(i, 1);
+    this.setState({
+      contacts: contacts
+    });
+  }
+
+  // Set state of updateName, this value will be used in the "editContact" function
+  updateNameFunc = (e) => {
+    this.setState({
+      updateName: e.target.value
+    })
+  }
+
+  // Set state of updateNumber, this value will be used in the "editContact" function
+  updateNumberFunc = (e) => {
+    this.setState({
+      updateNumber: e.target.value
+    })
+  }
+
+  // This is used to show the correct information when showing the popup
+  editContact = (index) => {
+    console.log('Edit contact with index: ' + index);
+    this.setState({
+      show: true,
+      editIndex: index
+    });
+    this.state.contacts.forEach((contact, i) => {
+      if (i === index) {
+        return (
+          this.setState({
+            updateName: contact.name,
+            updateNumber: contact.number
+          })
+        )
+      }
+    })
+  }
+
+  // In this function we are changing the contacts array with the updated values
+  updateSelectedContact = (e) => {
+    e.preventDefault();
+    let newContacts = this.state.contacts;
+    newContacts.forEach((contact, i) => {
+      if (i === this.state.editIndex) {
+        contact.name = this.state.updateName;
+        contact.number = this.state.updateNumber;
+      }
+    });
+    this.setState({
+      contacts: newContacts,
+      show: false,
+      updateName: '',
+      updateNumber: ''
+    })
+  }
+
+  fadeAwayEditForm = (e) => {
+    if (e.target.className === 'popup' || e.target.id === 'cross') {
+      this.setState({
+        show: false
+      })
+    }
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <h1>Contact book</h1>
+        <InsertForm
+          newName={this.state.newName}
+          newNumber={this.state.newNumber}
+          newNameFunc={this.newNameFunc}
+          newNumberFunc={this.newNumberFunc}
+          submitForm={this.submitForm} />
+
+        <Contacts
+          editContact={this.editContact}
+          removeContact={this.removeContact}
+          contacts={this.state.contacts} />
+
+        <EditForm 
+          updateSelectedContact={this.updateSelectedContact}
+          updateNameFunc={this.updateNameFunc}
+          updateNumberFunc={this.updateNumberFunc}
+          show={this.state.show}
+          updateName={this.state.updateName}
+          updateNumber={this.state.updateNumber}
+          fadeAwayEditForm={this.fadeAwayEditForm} />
       </div>
     );
   }
